bbc660e207d4f5ac4db49e93bac71c3ccb492402
Amir Choudhary <110274155+Amirchoudhary09@users.noreply.github.com>
2026-04-27 18:31:51 +0530

    Add files via upload

---
+++ depth.cpp
@@ -0,0 +1,19 @@
+﻿
+#include"depth.h"
+
+int WINAPI WinMain(HINSTANCE h, HINSTANCE, LPSTR, int) {
+	Depth* depth = new Depth();
+   depth->InitWindow(h);
+   depth->InitD3D();
+
+    MSG msg = {};
+    while (msg.message != WM_QUIT) {
+        if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else {
+            depth->Render();
+        }
+    }
+}
